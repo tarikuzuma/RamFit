f6228ded3a22075a11bfd40805cf3f478ab24b8f
Tarikuzuma <edwinjrgumba@gmail.com>
2023-06-19 11:10:40 +0800

    Added view_routine.py

--- dummy.py
+++ dummy.py
@@ -1,28 +1,53 @@
-import json
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-name_numbers = {}
+class CustomModel(QtCore.QAbstractListModel):
+    def __init__(self, data, parent=None):
+        super(CustomModel, self).__init__(parent)
+        self.data = data
 
-for i in range(1, 5):
-    file_path = f"profiles/mydata{i}.json"
+    def rowCount(self, parent=QtCore.QModelIndex()):
+        return len(self.data)
 
-    try:
-        with open(file_path, 'r') as f:
-            json_object = json.load(f)
-            name = json_object['info']['name']
-            print("Name:", name)
+    def data(self, index, role):
+        if not index.isValid():
+            return None
 
-            name_numbers[f"name_number{i}"] = name  # Store name in the dictionary
+        if role == QtCore.Qt.DisplayRole:
+            return self.data[index.row()].text
+        elif role == QtCore.Qt.DecorationRole:
+            return self.data[index.row()].image
 
-    except FileNotFoundError:
-        print(f"File {file_path} not found. Continuing to the next file.")
-        continue
+        return None
 
-# Print all name_numbers
-for i in range(1, 5):
-    var_name = f"name_number{i}"
-    value = name_numbers.get(var_name)
-    if value is not None:
-        print(f"{var_name}: {value}")
-    else:
-        value = None
-        print(f"{var_name} is not defined. Therefore: {value}")
+class ListItem:
+    def __init__(self, text, image):
+        self.text = text
+        self.image = image
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.list_view = QtWidgets.QListView(self)
+        self.setCentralWidget(self.list_view)
+
+        items = [
+            ListItem("Item 1", QtGui.QIcon("image1.png")),
+            ListItem("Item 2", QtGui.QIcon("image2.png")),
+            ListItem("Item 3", QtGui.QIcon("image3.png")),
+        ]
+
+        model = CustomModel(items)
+        self.list_view.setModel(model)
+
+        delegate = QtWidgets.QStyledItemDelegate(self.list_view)
+        self.list_view.setItemDelegate(delegate)
+
+        self.setWindowTitle("ListView with Images")
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
